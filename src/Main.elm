8c1e80ad3cd8287bad779f91af700ca88948c175
Matthieu JACQUOT <matthieu.jacquot.tech@protonmail.ch>
2020-06-09 09:51:32 +0200

    add invariants check

--- src/Main.elm
+++ src/Main.elm
@@ -11,6 +11,7 @@ import View exposing (..)
 type alias Model =
     { reqs : Dict Int ReqState
     , picked : Int
+    , failedInvariants : List String
     }
 
 
@@ -18,6 +19,7 @@ initialModel : Model
 initialModel =
     { reqs = Dict.fromList <| List.indexedMap Tuple.pair <| List.repeat 10 Pending
     , picked = -1
+    , failedInvariants = []
     }
 
 
@@ -54,6 +56,20 @@ activatedActions model =
                     []
 
 
+processedOnce : Model -> Maybe String
+processedOnce m =
+    if Dict.size (Dict.filter (\_ v -> v == Processed) m.reqs) > 1 then
+        Just "more than one processed"
+
+    else
+        Nothing
+
+
+invariantsCheck : Model -> List String
+invariantsCheck m =
+    List.filterMap identity [ processedOnce m ]
+
+
 view : Model -> Html Msg
 view model =
     div []
@@ -61,6 +77,7 @@ view model =
             [ h2 [] [ text "next move" ]
             , nextMoveDiv <| activatedActions model
             ]
+        , div [style "color" "red"] <| List.map (\x -> text x) <| invariantsCheck model
         , div (divAttrByState Pending) (divByState Pending model.reqs)
         , div (divAttrByState InProxy) (divByState InProxy model.reqs)
         , div (divAttrByState Processed) (divByState Processed model.reqs)
